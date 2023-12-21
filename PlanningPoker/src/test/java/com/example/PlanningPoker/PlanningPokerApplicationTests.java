2346249b7ebfca51cd6f22edcd4e90626ad38b8d
alban002 <aleksej.bans@stud.hn.de>
2023-12-21 10:16:02 +0100

    initial-commit
    initial commit

---
+++ PlanningPoker/src/test/java/com/example/PlanningPoker/PlanningPokerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.PlanningPoker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlanningPokerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
