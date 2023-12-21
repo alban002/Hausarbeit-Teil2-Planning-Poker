f43d17f4b58881e38eb8fb70b24da858e3380fea
alban002 <aleksej.bans@stud.hn.de>
2023-12-21 10:49:16 +0100

    Add-Second-Microservice
    Zweiter Microservice BacklogManagement

---
+++ BacklogManagement/BacklogManagement/src/main/java/com/example/BacklogManagement/BacklogManagementApplication.java
@@ -0,0 +1,13 @@
+package com.example.BacklogManagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BacklogManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BacklogManagementApplication.class, args);
+	}
+
+}
